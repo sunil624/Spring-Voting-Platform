1f70c7bd7d920f10d70c1fae9229bbff76c144e2
Sunil Shah <sunilshah7241@gmail.com>
2025-08-29 16:27:11 +0530

    First Commit

---
+++ src/main/java/com/example/votingapk/VotingApkApplication.java
@@ -0,0 +1,13 @@
+package com.example.votingapk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/votingapk/VotingApkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votingapk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
