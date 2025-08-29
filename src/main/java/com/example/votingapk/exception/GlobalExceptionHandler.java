c8f9c5037e431536bf914b65b3ca1e36a486fa0e
Sunil Shah <sunilshah7241@gmail.com>
2025-08-29 20:59:28 +0530

    Outside the RestControllers client seen the every object like views that we handling by using RresponseEntity.

--- src/main/java/com/example/votingapk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/votingapk/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.votingapk.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,18 +10,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = { NoSuchEmpExistsException.class })
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public ErrorResponse handleNoSuchEmpExistsException(NoSuchEmpExistsException noSuchEmpExistsException){
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    @ResponseBody
+    public ResponseEntity<ErrorResponse>  handleNoSuchEmpExistsException(NoSuchEmpExistsException noSuchEmpExistsException){
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), noSuchEmpExistsException.getMessage());
-        return errorResponse;
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = { EmpAlreadyExistsException.class })
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ResponseBody
-    public ErrorResponse handleEmpAlreadyExistsException(EmpAlreadyExistsException empAlreadyExistsException){
+//    @ResponseStatus(HttpStatus.CONFLICT)
+//    @ResponseBody
+    public ResponseEntity<ErrorResponse> handleEmpAlreadyExistsException(EmpAlreadyExistsException empAlreadyExistsException){
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(), empAlreadyExistsException.getMessage());
-        return errorResponse;
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 }
